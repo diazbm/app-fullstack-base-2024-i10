ed66ab21f0126fa927b9d6cee3507eccf3198b76
diazbm <105998374+diazbm@users.noreply.github.com>
2024-10-11 22:38:45 -0300

    Crear rutas para listar, crear, editar y eliminar dispositivos

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,7 +4,7 @@ var PORT    = 3000;
 
 var express = require('express');
 var app     = express();
-var utils   = require('./mysql-connector');
+var dbConnection   = require('./mysql-connector');
 
 // to parse application/json
 app.use(express.json()); 
@@ -13,24 +13,48 @@ app.use(express.static('/home/node/app/static/'));
 
 //=======[ Main module code ]==================================================
 
-app.get('/devices/', function(req, res, next) {
-    devices = [
-        { 
-            'id': 1, 
-            'name': 'Lampara 1', 
-            'description': 'Luz living', 
-            'state': 0, 
-            'type': 1, 
-        },
-        { 
-            'id': 2, 
-            'name': 'Ventilador 1', 
-            'description': 'Ventilador Habitacion', 
-            'state': 1, 
-            'type': 2, 
-        },
-    ]
-    res.send(JSON.stringify(devices)).status(200);
+app.get('/devices', function(req, res, next) {
+    // Ejecutamos una query para obtener la lista de dispositivos
+    devices = dbConnection.query('SELECT * FROM `Devices`', function (error, results) {
+        if (error) throw error;
+        res.send(JSON.stringify(results)).status(200);
+      });
+});
+
+app.post('/devices', function(req, res, next) {
+    const device = req.body
+    devices = dbConnection.query('INSERT INTO `Devices` SET ?', device, function (error) {
+        if (error) {
+            console.error('Error insertando datos:', JSON.stringify(device))
+        };
+        console.log(`Dispositivo ${JSON.stringify(device)} insertado correctamente.`)
+        res.send('').status(200);
+      });
+});
+
+app.put('/devices/:id', function(req, res, next) {
+    const device = req.body
+    const deviceId = req.params.id
+    const query = 'UPDATE `Devices` SET ? WHERE id='+deviceId
+    devices = dbConnection.query(query, device, function (error) {
+        if (error) {
+            console.error(`Error actualizando datos deviceId: ${deviceId}`, JSON.stringify(device))
+        };
+        console.log(`Dispositivo ${deviceId} actualizado correctamente.`)
+        res.send('').status(200);
+      });
+});
+
+app.delete('/devices/:id', function(req, res, next) {
+    const deviceId = req.params.id
+    const query = 'DELETE FROM `Devices` WHERE id='+deviceId
+    devices = dbConnection.query(query, function (error) {
+        if (error) {
+            console.error(`Error eliminado datos deviceId: ${deviceId}`)
+        };
+        console.log(`Dispositivo ${deviceId} eliminado correctamente.`)
+        res.send('').status(200);
+      });
 });
 
 app.listen(PORT, function(req, res) {
